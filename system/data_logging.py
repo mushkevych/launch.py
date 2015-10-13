0272835e668e9c081b783d229abb12d8b6e8cadd
Dan Mushkevych <bmushkevych@appannie.com>
2015-10-13 11:59:32 -0700

    - addressing default logger params

--- system/data_logging.py
+++ system/data_logging.py
@@ -9,14 +9,18 @@ from settings import settings
 class Logger(object):
     """ Logger presents wrapper around standard API enriched with formaters and roto handlers """
 
-    def __init__(self, file_name, log_tag,
-                 append_to_console=settings['under_test'], redirect_stdstream=not settings['under_test']):
+    def __init__(self, file_name, log_tag, append_to_console=None, redirect_stdstream=None):
         """
         :param file_name: path+name of the output file
         :param log_tag: tag that is printed ahead of every logged message
         :param append_to_console: True if messages should be printed to the terminal console
         :param redirect_stdstream: True if stdout and stderr should be redirected to this Logger instance
         """
+        if append_to_console is None:
+            append_to_console = settings['under_test']
+        if redirect_stdstream is None:
+            redirect_stdstream = not settings['under_test']
+
         self.logger = logging.getLogger(log_tag)
 
         if append_to_console:
@@ -26,11 +30,6 @@ class Logger(object):
             stream_formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
             stream_handler.setFormatter(stream_formatter)
             self.logger.addHandler(stream_handler)
-
-        if redirect_stdstream:
-            # While under_test, tools as xml_unittest_runner are doing complex sys.stdXXX reassignments
-            sys.stdout = self
-            sys.stderr = self
 
         if settings['debug']:
             self.logger.setLevel(logging.DEBUG)
@@ -43,6 +42,11 @@ class Logger(object):
                                                 datefmt='%Y-%m-%d %H:%M:%S')
         roto_file_handler.setFormatter(roto_file_formatter)
         self.logger.addHandler(roto_file_handler)
+
+        if redirect_stdstream:
+            # While under_test, tools as xml_unittest_runner are doing complex sys.stdXXX reassignments
+            sys.stderr = self
+            sys.stdout = self
 
     def get_logger(self):
         return self.logger
@@ -69,6 +73,7 @@ class Logger(object):
 if __name__ == '__main__':
     from system.process_context import ProcessContext
     from tests.ut_process_context import PROCESS_UNIT_TEST, register_unit_test_context
+
     register_unit_test_context()
 
     logger = ProcessContext.get_logger(PROCESS_UNIT_TEST)
