5839eb9ddcf096e47511722769c93f17a3cfc926
Bohdan Mushkevych <mushkevych@gmail.com>
2015-10-16 10:55:45 -0700

    - improving docklets

--- settings.py
+++ settings.py
@@ -2,15 +2,19 @@ ENVIRONMENT = '%ENVIRONMENT%'
 
 # folder locations, connection properties etc
 settings = dict(
-    process_prefix='Synergy',   # global prefix that is added to every process name started for synergy-data
+    process_prefix='Synergy',   # global prefix that is added to every process name started by launch.py
     process_cwd='/mnt/tmp',     # daemonized process working directory, where it can create .cache and other folders
 
     log_directory='/mnt/logs/launchpy/',
     pid_directory='/mnt/logs/launchpy/',
 
     perf_ticker_interval=60,    # seconds between performance ticker messages
-    debug=False,                # if True - logger is given additional "console" adapter
-    under_test=False
+
+    debug=False,                # if True, logger.setLevel is set to DEBUG. Otherwise to INFO
+
+    under_test=False            # marks execution of the Unit Tests
+                                # if True, a console handler for STDOUT and STDERR are appended to the logger.
+                                # Otherwise STDOUT and STDERR are redirected to .log files
 )
 
 # Update current dict with the environment-specific settings
